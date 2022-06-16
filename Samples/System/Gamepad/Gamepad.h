5a85e2f97f150953988bee0a0adf4f8ae22f3970
Chuck Walbourn <chuckw@microsoft.com>
2022-06-16 14:47:35 -0700

    Gamepad and GamepadVibration now include PC support

--- Samples/System/Gamepad/Gamepad.h
+++ Samples/System/Gamepad/Gamepad.h
@@ -14,20 +14,39 @@
 
 // A basic sample implementation that creates a D3D12 device and
 // provides a render loop.
-class Sample
+class Sample final : public DX::IDeviceNotify
 {
 public:
+
     Sample() noexcept(false);
+    ~Sample();
+
+    Sample(Sample&&) = default;
+    Sample& operator= (Sample&&) = default;
+
+    Sample(Sample const&) = delete;
+    Sample& operator= (Sample const&) = delete;
 
     // Initialization and management
-    void Initialize(HWND window);
+    void Initialize(HWND window, int width, int height);
 
     // Basic render loop
     void Tick();
 
+    // IDeviceNotify
+    void OnDeviceLost() override;
+    void OnDeviceRestored() override;
+
     // Messages
+    void OnActivated();
+    void OnDeactivated();
     void OnSuspending();
     void OnResuming();
+    void OnWindowMoved();
+    void OnWindowSizeChanged(int width, int height);
+
+    // Properties
+    void GetDefaultSize(int& width, int& height) const noexcept;
 
 private:
 
@@ -47,17 +66,18 @@ private:
     DX::StepTimer                               m_timer;
 
     //Gamepad states
-    Microsoft::WRL::ComPtr<IGameInput>			m_gameInput;
-    Microsoft::WRL::ComPtr<IGameInputReading>   m_reading;
-    std::vector<APP_LOCAL_DEVICE_ID>			m_deviceIds;
-    wchar_t							            m_deviceString[20];
-    std::wstring			                    m_buttonString;
-    double						                m_leftTrigger;
-    double                                      m_rightTrigger;
-    double                                      m_leftStickX;
-    double                                      m_leftStickY;
-    double                                      m_rightStickX;
-    double                                      m_rightStickY;
+    Microsoft::WRL::ComPtr<IGameInput>                      m_gameInput;
+    Microsoft::WRL::ComPtr<IGameInputReading>               m_reading;
+    std::vector<Microsoft::WRL::ComPtr<IGameInputDevice>>   m_devices;
+
+    wchar_t         m_deviceString[20];
+    std::wstring    m_buttonString;
+    double          m_leftTrigger;
+    double          m_rightTrigger;
+    double          m_leftStickX;
+    double          m_leftStickY;
+    double          m_rightStickX;
+    double          m_rightStickY;
 
     // DirectXTK objects.
     std::unique_ptr<DirectX::GraphicsMemory>    m_graphicsMemory;
@@ -66,14 +86,15 @@ private:
     // UI
     std::unique_ptr<DirectX::SpriteBatch>       m_batch;
     std::unique_ptr<DirectX::SpriteFont>        m_font;
+    std::unique_ptr<DirectX::SpriteFont>        m_smallFont;
     std::unique_ptr<DirectX::SpriteFont>        m_ctrlFont;
-    float                                       m_scale;
 
     Microsoft::WRL::ComPtr<ID3D12Resource>      m_background;
 
-    enum Descriptors
+    enum Descriptors : size_t
     {
         PrintFont,
+        TextFont,
         ControllerFont,
         Background,
         Count,
--- Samples/System/GamepadVibration/DeviceResources.h
+++ Samples/System/GamepadVibration/DeviceResources.h
@@ -1,21 +1,28 @@
 //
-// DeviceResources.h - A wrapper for the Direct3D 12.X device and swapchain
+// DeviceResources.h - A wrapper for the Direct3D 12/12.X device and swapchain
 //
 
 #pragma once
 
 namespace DX
 {
+    // Provides an interface for an application that owns DeviceResources to be notified of the device being lost or created.
+    interface IDeviceNotify
+    {
+        virtual void OnDeviceLost() = 0;
+        virtual void OnDeviceRestored() = 0;
+
+    protected:
+        ~IDeviceNotify() = default;
+    };
+
     // Controls all the DirectX device resources.
     class DeviceResources
     {
     public:
-        static constexpr unsigned int c_Enable4K_UHD = 0x1;
-
         DeviceResources(DXGI_FORMAT backBufferFormat = DXGI_FORMAT_B8G8R8A8_UNORM,
                         DXGI_FORMAT depthBufferFormat = DXGI_FORMAT_D32_FLOAT,
-                        UINT backBufferCount = 2,
-                        unsigned int flags = 0) noexcept(false);
+                        UINT backBufferCount = 2) noexcept(false);
         ~DeviceResources();
 
         DeviceResources(DeviceResources&&) = default;
@@ -26,11 +33,13 @@ namespace DX
 
         void CreateDeviceResources();
         void CreateWindowSizeDependentResources();
-        void SetWindow(HWND window) noexcept { m_window = window; }
+        void SetWindow(HWND window, int width, int height) noexcept;
+        bool WindowSizeChanged(int width, int height);
+        void HandleDeviceLost();
+        void RegisterDeviceNotify(IDeviceNotify* deviceNotify) noexcept { m_deviceNotify = deviceNotify; }
         void Prepare(D3D12_RESOURCE_STATES beforeState = D3D12_RESOURCE_STATE_PRESENT,
                      D3D12_RESOURCE_STATES afterState = D3D12_RESOURCE_STATE_RENDER_TARGET);
-        void Present(D3D12_RESOURCE_STATES beforeState = D3D12_RESOURCE_STATE_RENDER_TARGET,
-                     _In_opt_ const D3D12XBOX_PRESENT_PARAMETERS* params = nullptr);
+        void Present(D3D12_RESOURCE_STATES beforeState = D3D12_RESOURCE_STATE_RENDER_TARGET);
         void Suspend();
         void Resume();
         void WaitForGpu() noexcept;
@@ -40,6 +49,10 @@ namespace DX
 
         // Direct3D Accessors.
         auto                        GetD3DDevice() const noexcept          { return m_d3dDevice.Get(); }
+#ifdef _GAMING_DESKTOP
+        auto                        GetSwapChain() const noexcept          { return m_swapChain.Get(); }
+        auto                        GetDXGIFactory() const noexcept        { return m_dxgiFactory.Get(); }
+#endif
         HWND                        GetWindow() const noexcept             { return m_window; }
         D3D_FEATURE_LEVEL           GetDeviceFeatureLevel() const noexcept { return m_d3dFeatureLevel; }
         ID3D12Resource*             GetRenderTarget() const noexcept       { return m_renderTargets[m_backBufferIndex].Get(); }
@@ -53,7 +66,6 @@ namespace DX
         D3D12_RECT                  GetScissorRect() const noexcept        { return m_scissorRect; }
         UINT                        GetCurrentFrameIndex() const noexcept  { return m_backBufferIndex; }
         UINT                        GetBackBufferCount() const noexcept    { return m_backBufferCount; }
-        unsigned int                GetDeviceOptions() const noexcept      { return m_options; }
 
         CD3DX12_CPU_DESCRIPTOR_HANDLE GetRenderTargetView() const noexcept
         {
@@ -68,7 +80,11 @@ namespace DX
 
     private:
         void MoveToNextFrame();
+#ifdef _GAMING_XBOX
         void RegisterFrameEvents();
+#else
+        void GetAdapter(IDXGIAdapter1** ppAdapter);
+#endif
 
         static constexpr size_t MAX_BACK_BUFFER_COUNT = 3;
 
@@ -81,6 +97,10 @@ namespace DX
         Microsoft::WRL::ComPtr<ID3D12CommandAllocator>      m_commandAllocators[MAX_BACK_BUFFER_COUNT];
 
         // Swap chain objects.
+#ifdef _GAMING_DESKTOP
+        Microsoft::WRL::ComPtr<IDXGIFactory6>               m_dxgiFactory;
+        Microsoft::WRL::ComPtr<IDXGISwapChain3>             m_swapChain;
+#endif
         Microsoft::WRL::ComPtr<ID3D12Resource>              m_renderTargets[MAX_BACK_BUFFER_COUNT];
         Microsoft::WRL::ComPtr<ID3D12Resource>              m_depthStencil;
 
@@ -88,7 +108,9 @@ namespace DX
         Microsoft::WRL::ComPtr<ID3D12Fence>                 m_fence;
         UINT64                                              m_fenceValues[MAX_BACK_BUFFER_COUNT];
         Microsoft::WRL::Wrappers::Event                     m_fenceEvent;
+#ifdef _GAMING_XBOX
         D3D12XBOX_FRAME_PIPELINE_TOKEN                      m_framePipelineToken;
+#endif
 
         // Direct3D rendering objects.
         Microsoft::WRL::ComPtr<ID3D12DescriptorHeap>        m_rtvDescriptorHeap;
@@ -107,7 +129,7 @@ namespace DX
         D3D_FEATURE_LEVEL                                   m_d3dFeatureLevel;
         RECT                                                m_outputSize;
 
-        // DeviceResources options (see flags above)
-        unsigned int                                        m_options;
+        // The IDeviceNotify can be held directly as it owns the DeviceResources.
+        IDeviceNotify*                                      m_deviceNotify;
     };
 }
